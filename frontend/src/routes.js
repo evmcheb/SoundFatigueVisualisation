21eb2cdb2a261be26aa0d213d5fd4b605abfc628
aditiiby <aditimalu13@gmail.com>
2021-09-21 12:33:47 +0800

    Update routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,28 +4,36 @@ import Notifications from "views/Notifications.js";
 import TableList from "views/TableList.js";
 import Typography from "views/Typography.js";
 import UserProfile from "views/UserProfile.js";
+import Settings from "views/Settings";
 
-var routes = [
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    icon: "tim-icons icon-chart-pie-36",
-    component: Dashboard,
-    layout: "/admin",
-  },
-  {
-    path: "/userprofile",
-    name: "UserProfile",
-    icon: "tim-icons icon-chart-pie-36",
-    component: UserProfile,
-    layout: "/admin",
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    icon: "tim-icons icon-bell-55",
-    component: Notifications,
-    layout: "/admin",
-  }
+
+var routes = [{
+        path: "/dashboard",
+        name: "Dashboard",
+        icon: "tim-icons icon-chart-pie-36",
+        component: Dashboard,
+        layout: "/admin",
+    },
+    {
+        path: "/userprofile",
+        name: "UserProfile",
+        icon: "tim-icons icon-chart-pie-36",
+        component: UserProfile,
+        layout: "/admin",
+    },
+    {
+        path: "/notifications",
+        name: "Notifications",
+        icon: "tim-icons icon-bell-55",
+        component: Notifications,
+        layout: "/admin",
+    },
+    {
+        path: "/settings",
+        name: "settings",
+        icon: "tim-icons icon-align-center",
+        component: Settings,
+        layout: "/admin",
+    },
 ];
 export default routes;

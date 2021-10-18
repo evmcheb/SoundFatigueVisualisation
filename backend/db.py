fe860c72057a0c7f908856c1669fb807fae0a6c8
kese-gasbarri <kese.gasbarri@hotmail.com>
2021-10-18 15:14:31 +0800

    getting tests to work

--- backend/db.py
+++ backend/db.py
@@ -14,8 +14,12 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.orm import sessionmaker
+import sys
 
-engine = "sqlite:///../backend.db"
+if "pytest" in sys.modules:
+    engine = "sqlite:///backend.db"    
+else:
+    engine = "sqlite:///../backend.db"
 
 engine = create_engine(engine, connect_args={"check_same_thread":False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
